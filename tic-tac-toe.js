d70f4313d98622cdcf57aab4939b0ce90ff2cdd5
rahiemlampart@yahoo.com <rahiemlampart@yahoo.com>
2022-10-06 17:02:41 -0500

    Tic-Tac-Toe Second Stage

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,5 +1,32 @@
-document.addEventListener("DOMContentLoaded",function (e){
+document.addEventListener("DOMContentLoaded",function (){
    var board = document.getElementById("board").children;
-   for (x=0;x<9;x++)
-       board[x].setAttribute("class","square");
+   const board_s = [];
+   for (x=0;x<9;x++){
+       board[x].className = "square";
+       board[x].addEventListener("click",function(){
+         if (this.innerHTML == ("X"))
+         {
+           this.innerHTML = ("O");
+           this.classList.remove("X");
+           this.classList.add("O");
+         }
+         else if (this.innerHTML == ("O"))
+         {
+           this.innerHTML = ("X");
+           this.classList.remove("O");
+           this.classList.add("X");
+         }
+         else {
+           this.innerHTML = ("X");
+           this.classList.add("X");
+         }
+         console.log(this.classList);
+
+         //let square = document.getElementById("board").children[x];
+         //square.classList.add("X");
+       });
+       board[x].addEventListener("onmouseenter",function(){
+        this.classList.add("hover");
+         });
+  }
 })
