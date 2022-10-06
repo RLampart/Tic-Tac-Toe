072e498f34e597b81c7c0b9cf3e3f72d59337d8f
rahiemlampart@yahoo.com <rahiemlampart@yahoo.com>
2022-10-06 18:55:20 -0500

    Tic-Tac-Toe Fourth Stage

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,6 +1,9 @@
 document.addEventListener("DOMContentLoaded",function (){
    var board = document.getElementById("board").children;
-   const board_s = [];
+   let clicks = 0;
+   winner = document.getElementById("status");
+   const victory = [[0,1,2],[0,3,6],[1,4,7],[2,5,8],[3,4,5],[6,7,8],[0,4,8],[2,4,6]];
+   console.log(victory[0]);
    for (x=0;x<9;x++){
        board[x].className = "square";
        board[x].addEventListener("mouseover", function(event){
@@ -8,6 +11,7 @@ document.addEventListener("DOMContentLoaded",function (){
        board[x].addEventListener("mouseleave", function(event){
           this.classList.remove("hover")});
        board[x].addEventListener("click",function(event){
+
          if (this.innerHTML == ("X"))
          {
            this.innerHTML = ("O");
@@ -23,6 +27,28 @@ document.addEventListener("DOMContentLoaded",function (){
          else {
            this.innerHTML = ("X");
            this.classList.add("X");
+         }        
+         clicks++;
+         if (clicks>2){
+          for (let match of victory){
+            console.log(match);
+            if (board[match[0]].innerHTML == ("X") && board[match[1]].innerHTML == ("X") 
+            && board[match[2]].innerHTML == ("X")){
+              winner.innerHTML = ("Congratulations! X is the Winner!");
+              winner.className = "you-won";
+              break;
+        
+            }
+            else if (board[match[0]].innerHTML == ("O") && board[match[1]].innerHTML == ("O") 
+            && board[match[2]].innerHTML == ("O")){
+              winner.innerHTML = ("Congratulations! O is the Winner!");
+              winner.className = "you-won";
+              break;
+        
+            }
+            
+        
+          }
          }
          console.log(this.classList);
 
@@ -30,4 +56,6 @@ document.addEventListener("DOMContentLoaded",function (){
          //square.classList.add("X");
        });
   }
+  decision = document.getElementById("game");
+  
 })
